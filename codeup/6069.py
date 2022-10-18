5614d557f26989cdddd18e32ad842aa8656be829
Sanggyu Kim <ksg2194@naver.com>
2022-10-18 22:27:06 +0900

    add 6060-6070

---
+++ codeup/6069.py
@@ -0,0 +1,16 @@
+n = input()
+n = str(n)
+
+if n=="A" :
+  print('best!!!')
+else :
+  if n=="B" :
+    print('good!!')
+  else :
+    if n=="C" :
+      print('run!')
+    else :
+        if n=="D" :
+            print('slowly~')
+        else :
+            print("what?")
---
+++ codeup/6070.py
@@ -0,0 +1,14 @@
+n = input()
+n = int(n)
+
+if (n==12 or n==1 or n==2) :
+  print('winter')
+else :
+  if (n==3 or n==4 or n==5) :
+    print('spring')
+  else :
+    if (n==6 or n==7 or n==8) :
+      print('summer')
+    else :
+        if (n==9 or n==10 or n==11) :
+            print('fall')
